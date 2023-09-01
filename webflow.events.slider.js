3f808984dcb5380db323a293231a0ecea4278a7c
b-felinskyi <112482478+b-felinskyi@users.noreply.github.com>
2023-09-01 18:37:48 +0300

    Create webflow.events.slider.js

---
+++ webflow.events.slider.js
@@ -0,0 +1,28 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  var namespace = '.w-slider';
+
+  function slideChangeEvent(evt) {
+    var slider;
+    if($(evt.target).is(namespace)) {
+      slider = $(evt.target);
+    } else {
+      slider = $(evt.target).closest(namespace)
+    }
+    if(slider) {
+      $(slider).trigger('slider-event', $(slider).data(namespace));
+    }
+  }
+
+  var tap_selector = $.map(['.w-slider-arrow-left', '.w-slider-arrow-right', '.w-slider-dot'], function(s) { return namespace + ' ' + s; }).join(', ');
+
+  // listeners
+  $(document).off('tap' + namespace, tap_selector, slideChangeEvent).on('tap' + namespace, tap_selector, slideChangeEvent);
+  $(document).off('swipe' + namespace, namespace, slideChangeEvent).on('swipe' + namespace, namespace, slideChangeEvent);
+
+  // initial slide - manually trigger the event
+  $(namespace + ':visible').each(function(i, s) {
+    slideChangeEvent({ target: s });
+  });
+
+});
